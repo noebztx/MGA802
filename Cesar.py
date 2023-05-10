447a483209dc81dacadf7b5e753ad1668ca24644
nbazetoux <noe.bazetoux@gmail.com>
2023-05-10 14:52:42 -0400

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -1,11 +1,17 @@
+import string
+alphabet = string.ascii_lowercase
+
 # Ouvrir le fichier en mode lecture
-with open("message. txt", ' r') as f:
+#with open("message.txt", 'r') as f:
     # Lire le contenu du fichier
-    contenu = f.read()
+    # contenu = f.read()
+contenu = f.read()
+a = int(input("Entrez la clé :"))
+encrypte = ""
 
-
-a = input("Entrez la clé :")
+for k in [contenu]:
+    encrypte.append = alphabet[alphabet.find(k)+a]
 
 # On crée un fichier txt et on écrit dedans
 with open("message_encrypte.txt", "w") as fichier:
-    fichier.write()
+    fichier.write(encrypte)
