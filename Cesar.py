0ea82668bffeaca6440ac7c68ef609141e5d9eb1
nbazetoux <noe.bazetoux@gmail.com>
2023-05-10 15:21:46 -0400

    cesar

--- Cesar.py
+++ Cesar.py
@@ -2,16 +2,20 @@ import string
 alphabet = string.ascii_lowercase
 
 # Ouvrir le fichier en mode lecture
-#with open("message.txt", 'r') as f:
+with open("message.txt", 'r') as f:
     # Lire le contenu du fichier
-    # contenu = f.read()
-contenu = f.read()
+    contenu = f.read()
+
 a = int(input("Entrez la clé :"))
 encrypte = ""
 
-for k in [contenu]:
-    encrypte.append = alphabet[alphabet.find(k)+a]
+for k in contenu:
+    if k in alphabet:
+        encrypte += alphabet[(alphabet.find(k)+a) % 26]
+    else:
+        encrypte += k
 
+print(encrypte)
 # On crée un fichier txt et on écrit dedans
 with open("message_encrypte.txt", "w") as fichier:
     fichier.write(encrypte)
