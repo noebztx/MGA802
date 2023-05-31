9b68d2ee6e6e1fff21c5590cda984364e337ba81
nbazetoux <noe.bazetoux@gmail.com>
2023-05-31 18:38:53 -0400

    update

--- Pandas/mucipalités.py
+++ Pandas/mucipalités.py
@@ -18,23 +18,25 @@ population_moyenne_2021 = municipalites['Pop21'].mean()  # Calcul de la populati
 print("Population moyenne en 2016 :", population_moyenne_2016)
 print("Population moyenne en 2021 :", population_moyenne_2021)
 
-municipalites.loc[:, 'accroissement'] = 100 * (municipalites['Pop21'] - municipalites['Pop16']) / municipalites['Pop16'] # calcul de l'accroissement en %
+municipalites.loc[:, 'accroissement'] = 100 * (municipalites['Pop21'] - municipalites['Pop16']) / municipalites[
+    'Pop16']  # calcul de l'accroissement en %
 # Le résultat est assigné à une nouvelle colonne appelée 'accroissement' dans le DataFrame 'municipalites'.
+
 # Nuage de points : pourcentage d'accroissement de la population en fonction de la population en 2021
 plt.scatter(municipalites['Pop21'], municipalites['accroissement'])
 plt.xlabel('Population en 2021')
-plt.ylabel('Pourcentage d\'accroissement de la population')
-plt.title('Accroissement de la population en 2021')
+plt.ylabel('Accroissement de la population en %')
+plt.title('Accroissement de la population en 2021 par rapport à 2016')
 plt.show()
 
 # Classification des municipalités en 5 catégories selon leur population en 2021
 bins = [0, 2000, 10000, 25000, 100000, np.inf]  # Intervalle de population pour chaque catégorie
 labels = ['<2000', '2000-9999', '10000-24999', '25000-99999', '100000+']  # Étiquettes des catégories
-municipalites.loc[:, 'Population_Category'] = pd.cut(municipalites['Pop21'], bins=bins, labels=labels) # Le résultat
-# est assigné à une nouvelle colonne appelée 'Population_Category' dans le DataFrame 'municipalites'.
+municipalites.loc[:, 'Population_categorie'] = pd.cut(municipalites['Pop21'], bins=bins, labels=labels)  # Le résultat
+# est assigné à une nouvelle colonne appelée 'Population_categorie' dans le DataFrame 'municipalites'.
 
 # Diagramme en barres horizontales du nombre de municipalités dans chaque catégorie
-municipalites['Population_Category'].value_counts().sort_index().plot(kind='barh')
+municipalites['Population_categorie'].value_counts().sort_index().plot(kind='barh')
 plt.xlabel('Nombre de municipalités')
 plt.ylabel('Catégorie de population')
 plt.title('Répartition des municipalités par catégorie de population en 2021')
